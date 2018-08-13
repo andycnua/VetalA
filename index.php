87c9508193a30909dd18cc768f93f6798a693303
andycnua <andy.cnua@gmail.com>
2018-08-13 19:23:30 +0300

    Menu

--- index.php
+++ index.php
@@ -4,9 +4,21 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Веталь пхп разработчик</title>
-	<link rel="stylesheet" href="">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/main.css">
 </head>
 <body>
-	
+	<div class="header">
+		<div class="menu">
+			<div class="container">
+				<ul>
+					<li><a href="http://vetal.cn.ua/index.php"><h2>PlayStore</h2></a></li>
+					<li><a href="#">1</a></li>
+					<li><a href="#">2</a></li>
+					<li><a href="#">3</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
